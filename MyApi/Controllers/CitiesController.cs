5551c1baffe315001d1e31b8575f82a48dfd03da
Timbo Laptop <tbertenshaw@hotmail.com>
2017-08-21 17:14:40 +0100

    initial commit

---
+++ MyApi/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Cities")]
+    public class CitiesController : Controller
+    {
+        public CitiesController()
+        {
+
+        }
+        ////V1
+        //[HttpGet]
+        //[Route("getcities")]
+        //public IEnumerable<string> GetCities()
+        //{
+        //    return new string[] { "Seattle", "London" };
+        //}
+
+        //V2
+        [HttpGet]
+        [Route("getcities")]
+        public IEnumerable<string> GetCities()
+        {
+            return new string[] { "Seattle", "London", "Delhi", "Berlin" };
+        }
+    }
+}
---
+++ MyApi/ServiceRequestActionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi
+{
+    //internal class ServiceRequestActionFilter : ActionFilterAttribute
+    //{
+    //    public override void OnActionExecuting(HttpActionContext actionContext)
+    //    {
+    //        ServiceEventSource.Current.ServiceRequestStart(actionContext.ActionDescriptor.ActionName);
+    //    }
+
+    //    public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+    //    {
+    //        ServiceEventSource.Current.ServiceRequestStop(actionExecutedContext.ActionContext.ActionDescriptor.ActionName,
+    //            actionExecutedContext.Exception?.ToString() ?? string.Empty);
+    //    }
+    //}
+}
